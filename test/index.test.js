3d156d14c8733720c316c477a9481d34b7e7cf19
shinshin86 <beagles1986@gmail.com>
2020-06-24 22:22:07 +0900

    Fix test

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 test('Test', () => {
   execSync('node .');
 
-  const tarStream = require('../node_modules/tar-stream');
+  const tarStream = require('../spm_node_modules/tar-stream');
 
   expect(tarStream).toBeTruthy();
 });
